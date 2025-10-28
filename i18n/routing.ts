f9f488ea43e0dab55d89489deb2e3368f8343c31
marczlong <atupbcn@gmail.com>
2025-10-28 06:05:46 +0100

    Modelo con translate corregido

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,8 +1,9 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["es", "en"],
-  defaultLocale: "es",
+  locales: ["en", "es"],
 //   localePrefix: "as-needed",
+
+  defaultLocale: 'en'
 }); 
 
